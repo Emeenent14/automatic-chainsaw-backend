f64771d65ccc11e59d9bfaba45b7fab9b26d0c4a
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2025-05-11 22:12:23 +0100

    cors new origin

--- backend/settings.py
+++ backend/settings.py
@@ -123,7 +123,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "automatic-chainsaw-kappa.vercel.app"
 ]
 
 # For handling preflight requests
