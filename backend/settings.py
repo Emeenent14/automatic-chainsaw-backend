7fd53cdd5b148b26d189bbbd12081f93183c0f28
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2025-05-11 19:41:12 +0100

    switched databases to postgres: success

--- backend/settings.py
+++ backend/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newdb',
+        'USER': 'newuser',
+        'PASSWORD': 'newpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
