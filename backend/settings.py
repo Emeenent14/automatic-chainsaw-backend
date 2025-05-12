c6ceabcfa6a897aa115c9a30396d690affb169ac
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2025-05-12 15:30:22 +0100

    cors http/https issue fix

--- backend/settings.py
+++ backend/settings.py
@@ -123,7 +123,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
-    "http://automatic-chainsaw-kappa.vercel.app"
+    "http://automatic-chainsaw-kappa.vercel.app",
+    "https://automatic-chainsaw-kappa.vercel.app",
 ]
 
 # For handling preflight requests
